789c3be886c6dbb5e26769d194eef997ca788264
miraj gansi <mirajgansi122@gmail.com>
2026-01-04 22:04:57 +0545

    minor changes done

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -4,8 +4,8 @@ import { AuthController } from "../controllers/auth.controller";
 let authController = new AuthController();
 const router = Router();
 
-router.post("/register", authController.createUser)
-router.post("/login", authController.loginUser)
+router.post("/register", authController.register);
+router.post("/login", authController.login);
 // add remaning routes like login, logout, etc.
 
 export default router;
